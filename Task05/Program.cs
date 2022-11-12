ea7453a75c15716821652b9d98e75b4382c19333
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-12 16:06:50 +0300

    Add Task05

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -11,17 +11,17 @@ int number = Convert.ToInt32(Console.ReadLine());
 int count = -number;
 if (number >= 0)
 {
-while(count <= number)
-{
-    Console.Write(count+" ");
-    count++;
-}
+    while (count <= number)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
 }
 else
 {
     while (count >= number)
-{
-    Console.Write(count+" ");
-    count--;
-}
+    {
+        Console.Write(count + " ");
+        count--;
+    }
 }
