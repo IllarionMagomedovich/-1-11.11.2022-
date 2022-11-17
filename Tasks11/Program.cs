ce76ef98825983d5efb6b683c9f717e37fec946f
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-18 02:54:51 +0300

    Add Tasks12

--- Tasks11/Program.cs
+++ Tasks11/Program.cs
@@ -5,13 +5,13 @@
 // 918 -> 98
 
 
-int number = new Random().Next(100, 1000);
+int number = new Random().Next(101, 1000);
 Console.WriteLine($"Случайное число из отрезка 100 - 1000 --> {number}");
 
 int RemoveSecondDigit(int num)
 {
-    int firstDigit = num / 100;  
-    int secondDigit = num % 10; 
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
     int result = firstDigit * 10 + secondDigit;
     return result;
 }
---
+++ Tasks12/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить
+// является ли первое число кратным второму. Если число 1 не кратно числу 2,
+// то программа выводит остаток от деления.
+
+// 34, 5 = не кратно, остаток 4
+// 16, 4 = кратно
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Первый метод
+// bool Multiplicity (int num1, int num2)
+// {
+//      if (num1 % num2 == 0) return true;
+//      return false;
+// }
+// bool result = Multiplicity(number1, number2);
+// Console.WriteLine(result ? "Кратно" : $"Не кратно, остаток = {number1 % number2} ");
+
+if (number1 % number2 == 0) Console.WriteLine("Кратно");
+
+
+else 
+{ 
+     int result = number1 % number2;
+     if(result == 0); 
+    Console.WriteLine($"Не кратно, остаток  {result} ");
+}
