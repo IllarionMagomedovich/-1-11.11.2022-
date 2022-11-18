b8b31ad100160a0e30c786272feff2ad2073487f
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-18 14:53:23 +0300

    Add Tasks16

--- Tasks14/Program.cs
+++ Tasks14/Program.cs
@@ -7,26 +7,27 @@
 
 
 // Первый Метод
-// Console.WriteLine("Введите число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-//  bool Multiplicity (int number1)
-// {
-//      if (number1 % 7 == 0  & number1 % 23 == 0 ) return true;
-//      return false;
-// }
-// bool result = Multiplicity(num);
-// Console.WriteLine(result ? "Да" : $"Нет");
-
 Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int result = 0;
 
- if (num % 7 == 0 & num % 23 == 0) Console.WriteLine ("Да");
+ bool Multiplicity (int number1)
+{
+     if (number1 % 7 == 0  & number1 % 23 == 0 ) return true;
+     return false;
+}
+bool result = Multiplicity(num);
+Console.WriteLine(result ? "Да" : "Нет");
 
- else
- {
-    if(num % 7 != 0 & num % 23 != 0);
-    Console.WriteLine("Нет");
- }
+// Второй метод
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
+
+//  if (num % 7 == 0 & num % 23 == 0) Console.WriteLine ("Да");
+
+//  else
+//  {
+//     if(num % 7 != 0 & num % 23 != 0);
+//     Console.WriteLine("Нет");
+//  }
  
---
+++ Tasks16/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом дургого
+
+// 5, 25 = да
+// -4, 16 = да
+// 25, 5 = да
+// 8, 9 = нет
+
+// Первый метод
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2 * num2)
+// {
+//     Console.WriteLine("Да");
+// }
+
+// else if(num2 == num1 * num1)
+
+// {
+//     Console.WriteLine("Да");
+// }
+
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Второй метод
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Square (int number1, int number2)
+{
+    return (number1 == number2*number2) || (number2 == number1 * number1);
+}
+Console.WriteLine(Square(num1, num2)? "Да" : "Нет");
