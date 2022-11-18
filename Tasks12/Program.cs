1973cf0683373c64b7e811d7a1b3d3fc04b31668
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-18 14:59:13 +0300

    Add 12

--- Tasks12/Program.cs
+++ Tasks12/Program.cs
@@ -11,20 +11,22 @@ Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
 // Первый метод
-// bool Multiplicity (int num1, int num2)
-// {
-//      if (num1 % num2 == 0) return true;
-//      return false;
-// }
-// bool result = Multiplicity(number1, number2);
-// Console.WriteLine(result ? "Кратно" : $"Не кратно, остаток = {number1 % number2} ");
-
-if (number1 % number2 == 0) Console.WriteLine("Кратно");
-
-
-else 
-{ 
-     int result = number1 % number2;
-     if(result == 0); 
-    Console.WriteLine($"Не кратно, остаток  {result} ");
+bool Multiplicity (int num1, int num2)
+{
+     if (num1 % num2 == 0) return true;
+     return false;
 }
+bool result = Multiplicity(number1, number2);
+Console.WriteLine(result ? "Кратно" : $"Не кратно, остаток = {number1 % number2} ");
+
+
+// Второй метод
+// if (number1 % number2 == 0) Console.WriteLine("Кратно");
+
+
+// else 
+// { 
+//      int result = number1 % number2;
+//      if(result == 0); 
+//     Console.WriteLine($"Не кратно, остаток  {result} ");
+// }
