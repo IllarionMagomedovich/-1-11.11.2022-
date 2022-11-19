420148e08e5f748f4e9700d68f60ddd902560d16
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-19 19:20:42 +0300

    Add 10(HomeWork)

--- Tasks10(HomeWork)/Program.cs
+++ Tasks10(HomeWork)/Program.cs
@@ -9,11 +9,18 @@
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
- if(number < 100)  
-{
-    Console.WriteLine("Число не трехзначное");
-}  
+if (number > 0 & number < 100)  Console.WriteLine("Число не трехзначное");
 
+else if(number < -1)
+{
+   while(number < -1000)
+   {
+    number = number / 10;
+   }
+   number = number / 10;
+   number = number % 10;
+   Console.WriteLine(number);
+}
 else
 {
 int SecondDigit(int num)
@@ -21,13 +28,10 @@ int SecondDigit(int num)
      int secondDigit = num % 100;
      int result =secondDigit / 10;
      return result;
+ }
+ 
+Console.WriteLine(SecondDigit(number));
 }
- 
-
-Console.WriteLine(SecondDigit(number));      
-} 
- 
-
 
 
 
