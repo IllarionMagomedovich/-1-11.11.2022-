5959a1229da3e90fa583d57a65739136ae1f2a4c
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-26 02:08:18 +0300

    A

---
+++ Tasks26/Program.cs
@@ -0,0 +1,42 @@
+﻿// задача 26: Напишите программу, которая принимает на вход число и выдает
+// количество цифр в числе.
+
+// 456 = 3
+// 78 = 2
+// 89126 = 5
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitLength(int num)
+{
+    int n = 1;
+    for(int i = 0; i < 10 ; i++ )
+    {
+        num = num / 10;
+        if(num != 0) n++;
+        else break;
+
+    }
+    return n;
+}
+int digitlength = DigitLength(number);
+Console.WriteLine(digitlength);
+
+// Способ через цикл while
+int Digits(int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count == 0 ? 1 : count;
+}
+if (number < 0) number = -number;
+int result = Digits(number);
+Console.WriteLine($"Количство цифр в числе {number} = {result} ");
+
+
+Console.WriteLine(Math.Abs(-123));
---
+++ Tasks28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдает
+// произведение чисел от 1 до N.
+
+// 4 = 24
+// 5 = 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Multiplication(int num)
+{   
+    int com = 1;
+    for( int i = 1; i <= num; i++)
+    {
+        com = com * i;
+    }
+    return com;
+}
+if(number > 0)
+{
+    int multiplication = Multiplication(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
+}
+
+
