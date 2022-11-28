11fda8ee1d7b40430103ddf3266945580f256cbf
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-11-29 01:54:14 +0300

    Add Tasks27

---
+++ Tasks27(HomeWork)/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдает сумму цифр в числе.
+
+// 452 = 11
+// 82 = 10
+// 9012 = 12
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int firstdigit = 0;
+    int result = 0;
+    while(num > 0)
+    {
+        firstdigit = num % 10;
+        result = result + firstdigit;
+        num = num / 10;
+        
+    }
+    return result;
+}
+if(number < 0) number = -number;
+int sumdigit = SumDigit(number);
+Console.WriteLine($"Сумма чисел  {number} = {sumdigit} ");
