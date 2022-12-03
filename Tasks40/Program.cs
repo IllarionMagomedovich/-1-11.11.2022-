ad9d69c6c7d86d3edb32ce215a8341d842d4fbad
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-12-04 01:11:35 +0300

    Add

---
+++ Tasks40/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 40:
+// Напишите программу, которая принимает на вход три
+// числа и проверяет, можеттли существовать 
+// треугольник с сторонами такой длины.
+
+// Теорема о неравенстве треугольника: каждая сторона
+// треугольника меньше суммы двух других сторон.
+
+bool Triagnle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < a + b;
+}
+
+Console.WriteLine("Введите сторону треугольника");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону треугольника");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Triagnle(a, b, c) ? "Да" : "Нет");
+
---
+++ Tasks42/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 42:
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+// 45 = 10101
+// 3 = 11
+// 2 = 10
+
+// string Binary(int numb)
+// {
+//     string bin = string.Empty;
+//     while(numb > 0)
+//     {
+//         bin = numb % 2 + bin;
+//         numb /= 2;
+//     }
+//     return bin;
+// }
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(Binary(number));
+
+
+
+// int[] CreateArrayRndInt (int size, int min, int max)
+// {
+//     int [] arr = new int[size];
+//     Random rnd = new Random();
+    
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+void PrintArray (int[] arr, string beginStr, string separatorElems, string endStr)
+
+{
+    Console.Write(beginStr);
+     for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length -1) Console.Write($"{arr[i]}{separatorElems} ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine(endStr);
+}
+
+int[] Binary(int numb)
+{
+    int numb1 = numb;
+    int digits = 0;
+    while(numb > 0)
+    {
+        numb1 /= 2;
+        digits += 1;
+    }
+    int[] bin = new int[digits];
+    for(int i = 0; i < bin.Length; i++)
+    {
+        bin[i] = numb % 2;
+        numb /= 2;   
+    }
+    Array.Reverse(bin);
+    return bin;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] binary = Binary(number);
+PrintArray(binary, "", "", "");
