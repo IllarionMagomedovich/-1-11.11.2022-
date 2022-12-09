8ca3b190564aedb260f83d59b699341c107517ec
IllarionMagomedovich <vadim.evenko98@mail.ru>
2022-12-09 03:52:54 +0300

    Add

---
+++ Tasks52(HomeWork)/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+
+    }
+}
+
+
+double[] SumDigits(int[,] matrix)
+{
+    double[] sumDigits = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+           sum += matrix[i, j];
+        }
+        sumDigits [j] = Math.Round(sum / matrix.GetLength(0), 2);
+    }
+    return sumDigits;
+
+}
+void PrintArray(double[] arr)
+{
+    Console.Write("");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("");
+}
+
+
+int[,] creatMatrixRndInt = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(creatMatrixRndInt);
+double[] array = SumDigits(creatMatrixRndInt);
+Console.WriteLine("Среднее арифметическое элементов в столбцах: ");
+PrintArray(array);
+
